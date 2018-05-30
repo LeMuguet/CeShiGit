e61caeac7dd7a2d95c700d7899dac27070a8410a
LeMuguet <1825246719@qq.com>
2018-05-30 14:22:41 +0800

    Initial Commit

---
+++ CeShiGit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CeShiGit
+//
+//  Created by 申慧锋 on 2018/5/30.
+//  Copyright © 2018年 申慧锋. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CeShiGit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CeShiGit
+//
+//  Created by 申慧锋 on 2018/5/30.
+//  Copyright © 2018年 申慧锋. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
